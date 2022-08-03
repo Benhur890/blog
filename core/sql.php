b606250fa097d509c35c3aa7037041ff4336a764
Benhur <benhurcontel@gmail.com>
2022-08-03 11:21:52 -0300

    aa

--- core/sql.php
+++ core/sql.php
@@ -1,4 +1,35 @@
 <?php
+
+function insert (string $entidade, array $dados) : String 
+{
+$instrucao = "INSERT INTO {$entidade}";
+
+$campos = implode(', ', array_keys($dados));
+$valores = implode(', ', array_values($dados));
+
+$instrucao.= "({$campos})";
+$instrucao .= "VALUES ({$valores})";
+
+return $instrucao;
+}
+
+/* */
+
+function delete (string $entidade, array $criterio = []) : String
+{
+    $instrucao = "DELETE {$entidade}";
+
+    if(!empty($criterio)){
+        $instrucao .=' WHERE ';
+
+        foreach($criterio as $expressao){
+            $instrucao .=' ' . implode(' ', $expressao);
+        }
+    }
+
+    return $instrucao;
+}
+
 function update (string $entidade, array $dados, array $criterio = []) : string
 {
     $instrucao = "UPDATE{$entidade}";
@@ -21,7 +52,7 @@ function update (string $entidade, array $dados, array $criterio = []) : string
 
 function  select (string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
 {
-    $instrucao = "SELECT" . implode(',',$campos);
+    $instrucao = " SELECT" . implode(',',$campos);
     $instrucao .= "FROM {$entidade}";
 
     if(!empty($criterio)){
@@ -36,4 +67,25 @@ function  select (string $entidade, array $campos, array $criterio = [], string 
     }
     return $instrucao;
 }
+
+function select (string $entidade, array $campos, array $criterio = [], string $ordem = null) : string
+{
+    $instrucao = "SELECT " . IMPLODE (', ' ,$campos);
+    $instrucao .=" FROM {$entidade}";
+
+    if(!empty($criterio)){
+        $instrucao .= ' WHERE ';
+
+        foreach($criterio as $expressao){
+            $instrucao .=' ' . implode(' ', $expressao);
+        }
+    }
+
+    if(!empty($ordem)){
+        $instrucao .= "ORDER BY $ordem ";
+    }
+
+    return $instrucao;
+}
+
 ?>
